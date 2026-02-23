0921b09202ae417f368e67073ef520a5fe1f4c3a
devjihad <mdhajihad609@gmail.com>
2026-02-23 19:55:47 +0600

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+
+for(let i= 1 ; i <=10 ; i++){
+    let result = i*3
+    let value = `${i} *${3} = ${result}`
+    console.log(value)
+}
+
+
+let sum =0
+for (let i = 1; i <= 500; i++){
+    if (i % 2 === 0){
+        sum += i
+        
+    }
+}
+console.log(sum )
