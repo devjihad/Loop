0c0d2842bd081b2745c44a1785a4931f97d3af58
devjihad <mdhajihad609@gmail.com>
2026-02-23 21:19:27 +0600

    done

--- index.js
+++ index.js
@@ -2,15 +2,35 @@
 for(let i= 1 ; i <=10 ; i++){
     let result = i*3
     let value = `${i} *${3} = ${result}`
-    console.log(value)
+    // console.log(value)
 }
 
 
-let sum =0
+let oddnumber =0
 for (let i = 1; i <= 500; i++){
     if (i % 2 === 0){
-        sum += i
+        oddnumber += i
         
     }
 }
-console.log(sum )
+// console.log(oddnumber )
+
+let evennumber = 0
+
+for (let i= 1 ; i <=500; i++ ){
+    if(i % 1 ===0){
+        evennumber += i
+    }
+}
+// console.log(evennumber)
+for ( let a= 1 ; a <= 10 ; a++){
+    console.log('')
+     
+    for ( let b = 1 ; b <= 10 ; b++){
+      let result = (b * a )
+      console.log(`${a} * ${b} = ${result}`)
+    }
+       
+
+        
+}
